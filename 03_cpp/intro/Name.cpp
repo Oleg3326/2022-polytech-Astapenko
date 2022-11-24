52c19e4287605f5ab97de45d801efc7ee471bd76
Oleg3326 <olson3326@gmail.com>
2022-11-24 20:16:12 +0300

    feat(intro):add Name.cpp

---
+++ 03_cpp/intro/Name.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string username;
+    cout << "Enter your name, please:";
+    cin >> username; 
+    cout << "\nHello, " << username << "!";
+    return 0;
+}
